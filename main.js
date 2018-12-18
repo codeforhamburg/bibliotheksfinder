1cb467ca5850a7a5e4db9b7a0c2690f4d5f165cc
kigrantz <44206603+kigrantz@users.noreply.github.com>
2018-12-18 10:44:44 +0100

    icon größe

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ map.on("load", function() {
         "source": "hamburg",
         "layout": {
             "icon-image": "star-15",
-            "icon-size": 6.25,
+            "icon-size": 3.25,
             "icon-allow-overlap": true
         },
         // "paint": {
