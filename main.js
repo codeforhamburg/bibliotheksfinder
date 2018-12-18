8b20c3effa65dab3ac359b5d297b417b1add70c2
kigrantz <44206603+kigrantz@users.noreply.github.com>
2018-12-18 14:47:39 +0100

    email

--- main.js
+++ main.js
@@ -113,7 +113,7 @@ map.on("load", function() {
                 '<p>' + streetAddress + '</p>' + 
                 '<p>' + postalCode + ' ' + Locality + '</p>' + 
                 '<a href="' + url + '" target=_"blank">' + '<img src="./images/telephone.png">' + '</a>' + ' ' + '<a href="tel:' + telephone + '">' + telephone + '</a>' + '<br>' +
-                '<a href="' + url + '" target=_"blank">' + '<img src="./images/image-link.svg">' + '</a>' + ' ' + '<a href="mailto:' + email + '">' + email + '</a>' + '<br>' +
+                '<a href="' + url + '" target=_"blank">' + '<img src="./images/email.png">' + '</a>' + ' ' + '<a href="mailto:' + email + '">' + email + '</a>' + '<br>' +
                 // '<i class="fa fa-file-image-o" aria-hidden="true"></i>' + 
                 // '<img src="/images/image-link.svg">' + '<br>' + 
                 // '<a href="' + url + '" target=_"blank">' + url + '</a>' + '<br>' + 
