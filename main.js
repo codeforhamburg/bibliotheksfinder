eb7d78a210c1d1a0f048f2b6ac86c3d3e46aa21d
marinaschmitchen <44359539+marinaschmitchen@users.noreply.github.com>
2018-12-18 14:18:59 +0100

    auskomm

--- main.js
+++ main.js
@@ -115,8 +115,8 @@ map.on("load", function() {
                 '<a href="tel:' + telephone + '">' + telephone + '</a>' + '<br>' +
                 '<a href="mailto:' + email + '">' + email + '</a>' + '<br>' +
                 // '<i class="fa fa-file-image-o" aria-hidden="true"></i>' + 
-                '<img src="/images/image-link.svg">' + '<br>' + 
-                '<a href="' + url + '" target=_"blank">' + url + '</a>' + '<br>' + 
+                // '<img src="/images/image-link.svg">' + '<br>' + 
+                // '<a href="' + url + '" target=_"blank">' + url + '</a>' + '<br>' + 
                 '<a href="' + url + '" target=_"blank">' + '<img src="./images/image-link.svg">' + '</a>' + ' ' + '<a href="' + url + '" target=_"blank">' + url + '</a>' + '<br>'
                 )
             .addTo(map);
